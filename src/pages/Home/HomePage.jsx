1b12d09e8f6636a708888b1528a7074a1ae24aa2
Alenochka2003 <alena.filina@icloud.com>
2024-08-06 18:48:11 +0200

    add dogs

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,3 +1,33 @@
+// import React from 'react';
+
+// import CheckOutButton from '../../components/Buttons/CheckOutButton/CheckOutButton';
+// import CategoriesHeader from '../../components/CategoriesHeader/CategoriesHeader';
+// import DiscountForm from '../../components/DiscountForm/DiscountForm';
+// import Sale from '../../components/Sale/Sale';
+// import styles from './HomePage.module.css';
+
+// import backgroundImg from '../../assets/images/main-bg.jpg';
+
+// function HomePage() {
+//   return (
+//     <div>
+//       <div className="globalContainer">
+//         <div className={styles.contentStyle}>
+//           <h1>Amazing Discounts <br/> on Pets Products!</h1>
+//           <CheckOutButton />
+//         </div>
+//       </div>
+//       <div className={styles.mainBgStyle} style={{ backgroundImage: `url(${backgroundImg})` }}></div>
+
+//       <CategoriesHeader />
+//       <DiscountForm />
+//       <Sale />
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+
 import React from 'react';
 
 import CheckOutButton from '../../components/Buttons/CheckOutButton/CheckOutButton';
@@ -7,6 +37,7 @@ import Sale from '../../components/Sale/Sale';
 import styles from './HomePage.module.css';
 
 import backgroundImg from '../../assets/images/main-bg.jpg';
+import animation from '../../assets/images/animation.gif'; // Замените на путь к вашей анимации
 
 function HomePage() {
   return (
@@ -17,7 +48,9 @@ function HomePage() {
           <CheckOutButton />
         </div>
       </div>
-      <div className={styles.mainBgStyle} style={{ backgroundImage: `url(${backgroundImg})` }}></div>
+      <div className={styles.mainBgStyle} style={{ backgroundImage: `url(${backgroundImg})` }}>
+        <img src={animation} alt="Animation" className={styles.animationStyle} />
+      </div>
 
       <CategoriesHeader />
       <DiscountForm />
@@ -27,3 +60,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
